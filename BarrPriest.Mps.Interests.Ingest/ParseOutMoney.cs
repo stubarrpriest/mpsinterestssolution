89cb44efe2ef9f8846942250026aa29787900f47
stuart <stuart_a_barr@hotmail.com>
2020-05-04 11:52:22 +0100

    Initialise project and solution

---
+++ BarrPriest.Mps.Interests.Ingest/ParseOutMoney.cs
@@ -0,0 +1,16 @@
+using System;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Host;
+using Microsoft.Extensions.Logging;
+
+namespace BarrPriest.Mps.Interests.Ingest
+{
+    public static class ParseOutMoney
+    {
+        [FunctionName("ParseOutMoney")]
+        public static void Run([QueueTrigger("interests", Connection = "")]string myQueueItem, ILogger log)
+        {
+            log.LogInformation($"C# Queue trigger function processed: {myQueueItem}");
+        }
+    }
+}

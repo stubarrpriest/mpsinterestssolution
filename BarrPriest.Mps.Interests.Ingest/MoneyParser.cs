44a28cc26e5cbc14ccda0d4198337df08517cb71
stuart <stuart_a_barr@hotmail.com>
2020-05-04 12:24:23 +0100

    Add test project and dependencies

---
+++ BarrPriest.Mps.Interests.Ingest/MoneyParser.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BarrPriest.Mps.Interests.Ingest
+{
+    public class MoneyParser
+    {
+        public string Result()
+        {
+            return string.Empty;
+        }
+    }
+}
---
+++ BarrPriest.Mps.Interests.Tests/Ingest/MoneyParserTests.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using BarrPriest.Mps.Interests.Ingest;
+using NUnit.Framework;
+
+namespace BarrPriest.Mps.Interests.Tests.Ingest
+{
+    [TestFixture]
+    public class MoneyParserTests
+    {
+        [Test]
+        public void CanGetMoneyFromHtml()
+        {
+            var parser = new MoneyParser();
+
+            Assert.AreEqual(string.Empty, parser.Result());
+        }
+    }
+}

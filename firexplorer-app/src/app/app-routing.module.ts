63612349783b97f440df5c6ed6cb509fda1cc09b
stuart <stuart_a_barr@hotmail.com>
2020-06-11 10:29:49 +0100

    Add interests list component and update routing

--- firexplorer-app/src/app/app-routing.module.ts
+++ firexplorer-app/src/app/app-routing.module.ts
@@ -1,8 +1,13 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { InterestsComponent } from './interests/interests.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+      { path: 'list', component: InterestsComponent },
+      { path: '', redirectTo: 'list', pathMatch: 'full' },
+      { path: '**', redirectTo: 'list', pathMatch: 'full' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- firexplorer-app/src/app/app.module.ts
+++ firexplorer-app/src/app/app.module.ts
@@ -3,10 +3,12 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { InterestsComponent } from './interests/interests.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    InterestsComponent
   ],
   imports: [
     BrowserModule,
